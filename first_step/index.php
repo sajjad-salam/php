af048768fdd30805833291f9ae59a7e4ae8ce989
sajjad_salam <wtgd5777@gmail.com>
2022-12-05 21:30:13 +0300

    this is a first step and include echo and comment

---
+++ first_step/index.php
@@ -0,0 +1,31 @@
+<!-- this is a comment -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>
+        <?= 'My First PHP Page'; ?>
+    </title>
+</head>
+<body>
+    <div>
+        <?php
+        echo 'We Love';
+        ?>
+    </div>
+    <div>
+        <?php
+        echo 'Elzero Channel';
+        #echo 'Prevent Me From Running Please';
+        //echo 'Prevent Me From Running Please';
+        /*echo 'Prevent Me From Running Please';*/
+        // ## First Comment
+        # // # Second Comment
+        /* /* /* Third Comment */
+        ////// Fourth Comment
+        ?>
+    </div>
+</body>
+</html>
