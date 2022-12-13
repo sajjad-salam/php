2bf5a2d97c66754e1748f0ce9c4b72bdb9561503
sajjad_salam <wtgd5777@gmail.com>
2022-12-13 10:39:13 +0300

    s

--- third_step/Predefined_var.php
+++ third_step/Predefined_var.php
@@ -5,5 +5,6 @@ echo $_POST["user"];
 <!-- ======================== -->
 <form action="" , method="post">
     <input type="text"  name="user">
+    <input type="image" src="" alt="">
     <input type="submit" value="send">
 </form>
