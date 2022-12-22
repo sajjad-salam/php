680de62fb8e734cd0dbc7619cc44b59c07f0b943
sajjad_salam <wtgd5777@gmail.com>
2022-12-22 17:08:16 +0300

    Assigned

---
+++ Assigned_on_youtube/Assigned1.php
@@ -0,0 +1,52 @@
+<?php
+//this is a first comment
+?>
+<!-- this is a comment  -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= "My First PHP Page" ?></title>
+</head>
+
+<body>
+    <div><?php echo "We Love"; ?></div>
+    <div><?php echo "Elzero Channel"; ?></div>
+</body>
+
+</html>
+
+
+<!-- //==========2=========================
+ -->
+<?php
+#echo 'Prevent Me From Running Please';
+//echo 'Prevent Me From Running Please';
+/*echo 'Prevent Me From Running Please';*/
+?>
+
+
+
+<!-- =========3============ -->
+
+
+<?php
+/*
+My Application
+Version 1.0
+Created By Elzero
+*/
+
+?>
+<!-- // ===4== -->
+
+<?php
+// ## First Comment
+# // # Second Comment
+/* /* /* Third Comment */
+////// Fourth Comment
+
+//no problem
