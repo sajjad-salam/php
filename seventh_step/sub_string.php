7a99045f0a91fc5415bd7c49e87bb973579a1b18
sajjad_salam <wtgd5777@gmail.com>
2022-12-26 20:06:57 +0300

    .

--- seventh_step/sub_string.php
+++ seventh_step/sub_string.php
@@ -5,13 +5,13 @@ exchange according to a specific index
 */
 
 
-echo  substr_replace("onetow",1,0);//1
+echo  substr_replace("onetwo",1,0);//1
 echo "<br>";
-echo  substr_replace("onetow",1,3);//one1
+echo  substr_replace("onetwo",1,3);//one1
 echo "<br>";
-echo  substr_replace("onetow",1,4);//onet1
+echo  substr_replace("onetwo",1,4);//onet1
 echo "<br>";
-echo  substr_replace("onetow",1,-1);//oneto1
+echo  substr_replace("onetwo",1,-1);//oneto1
 echo "<br>";
 
 
