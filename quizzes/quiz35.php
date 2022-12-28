3dde7fa7e99f4a89a97f8d2dfbde7e550cc138f2
sajjad_salam <wtgd5777@gmail.com>
2022-12-28 13:03:21 +0300

    .

--- quizzes/quiz35.php
+++ quizzes/quiz35.php
@@ -1,18 +1,14 @@
 <?php
-    $nums = [11, 2, 10, 7, 20, 50];
-    function sumu(...$n)
-    {
-        $r=0;
-        foreach ($n as $key => $value) {
-            # code...
-
-            $r+=$value;
-        }
-        return $r;
+$nums = [11, 2, 10, 7, 20, 50];
+function sumu(...$n)
+{
+    $r = 0;
+    foreach ($n as $key => $value) {
+        $r += $value;
     }
-    echo sumu(...$nums);
+    return $r;
+}
+echo sumu(...$nums);
 
     // Output
     // 100
-
-
