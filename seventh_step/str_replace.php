7b27355875010231f847244ae38ead902087753f
sajjad_salam <wtgd5777@gmail.com>
2022-12-28 13:39:41 +0300

    string replace manual function

--- seventh_step/str_replace.php
+++ seventh_step/str_replace.php
@@ -13,5 +13,18 @@ echo "<br>";
 echo  str_ireplace("M","0","sajjadsalamm",$i);//sajjadsala00//Letters are insensitive
 echo "<br>";
 
-
+?>
 // manual
+<?php
+function stringreplace($st_r, $rep, $strg)
+{
+    
+    for ($i=0; $i < strlen($strg); $i++) { 
+
+        if ($strg[$i]==$st_r) {
+            $strg[$i] = $rep;
+        }
+    }
+    return $strg;
+}
+echo  stringreplace("m", "0", "malamm");
